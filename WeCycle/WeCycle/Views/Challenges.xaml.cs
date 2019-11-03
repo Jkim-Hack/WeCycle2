95b0d448768f1b8d3eb1b85052d34a573cc082ed
Jkim-Hack <johnkim1108@gmail.com>
2019-11-03 07:09:24 -0500

    Added minor changes

--- WeCycle/WeCycle/Views/Challenges.xaml.cs
+++ WeCycle/WeCycle/Views/Challenges.xaml.cs
@@ -15,6 +15,35 @@ namespace WeCycle
         public Challenges()
         {
             InitializeComponent();
+            CollectionView collectionView = new CollectionView();
+            collectionView.SetBinding(ItemsView.ItemsSourceProperty, "Challeneges");
+
+            collectionView.ItemTemplate = new DataTemplate(() =>
+            {
+                Grid grid = new Grid { Padding = 10 };
+                grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+                grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+
+                Image image = new Image { Aspect = Aspect.AspectFill, HeightRequest = 60, WidthRequest = 60 };
+                image.SetBinding(Image.SourceProperty, "ImageUrl");
+
+                Label nameLabel = new Label { FontAttributes = FontAttributes.Bold };
+                nameLabel.SetBinding(Label.TextProperty, "Name");
+
+                Label descLabel = new Label { FontAttributes = FontAttributes.Italic, VerticalOptions = LayoutOptions.End };
+                descLabel.SetBinding(Label.TextProperty, "Description");
+
+                Grid.SetRowSpan(image, 2);
+
+                grid.Children.Add(image);
+                grid.Children.Add(nameLabel, 1, 0);
+                grid.Children.Add(descLabel, 1, 1);
+
+                return grid;
+            });
+
         }
         void changePg(object sender, SwipedEventArgs e)
         {
